8aae450d460474a1916b637ecdb31fca6d87404b
Valentin Beletti <valentinbeletti7@hotmail.com>
2024-01-03 14:38:44 -0300

    carousel

--- src/Views/CasosExito/index.jsx
+++ src/Views/CasosExito/index.jsx
@@ -5,16 +5,24 @@ import Carousel from '../../components/Carousel';
 import TarjetaTesti from '../../components/Testimonio';
 
 const CasosExito = () => {
+  const imagenesCarrusel = [
+    "src/assets/logo1.jpg",
+    "src/assets/logo2.jpg",
+    "src/assets/logo3.jpg",
+    "src/assets/logo4.jpg",
+    "src/assets/logo5.jpg",
+    "src/assets/logo6.jpg",
+  ];
+
   return (
     <>
-    <Panel imagen="src/assets/buildings-1850129_1280.jpg"  seccion="Casos de Exito" subSeccion=" "/>
-    <Carousel titulo="Empresas y Cooperativas que confian en nuestros Servicios"/>
-    <div className={styles.contenido + ' grid grid-cols-3'}>
-    <TarjetaTesti img="src/assets/imagen1.jpg" nombre="Valentin Beletti" empresa="Mercado Libre" texto="Porque si no exportara de tu casa un cabello, que es grande, y nada de lo cual traería a tu casa por poco tiempo, yo sería mi hogar, o sería el más pequeño."/>
-    <TarjetaTesti img="src/assets/persona-e1533759204552.jpg" nombre="Gonzalo Pagliano" empresa="Amazon" texto="Porque si no exportara de tu casa un cabello, que es grande, y nada de lo cual traería a tu casa por poco tiempo, yo sería mi hogar, o sería el más pequeño."/>
-    <TarjetaTesti img="src/assets/2165947w620.jpg" nombre="Matias Lorenzatti" empresa="Apple" texto="Porque si no exportara de tu casa un cabello, que es grande, y nada de lo cual traería a tu casa por poco tiempo, yo sería mi hogar, o sería el más pequeño."/>
-
-     </div>
+      <Panel imagen="src/assets/buildings-1850129_1280.jpg" seccion="Casos de Exito" subSeccion=" " />
+      <Carousel titulo="Empresas y Cooperativas que confían en nuestros Servicios" imagenes={imagenesCarrusel} />
+      <div className={styles.contenido + ' grid grid-cols-3'}>
+        <TarjetaTesti img="src/assets/imagen1.jpg" nombre="Valentin Beletti" empresa="Mercado Libre" texto="Porque si no exportara de tu casa un cabello, que es grande, y nada de lo cual traería a tu casa por poco tiempo, yo sería mi hogar, o sería el más pequeño." />
+        <TarjetaTesti img="src/assets/persona-e1533759204552.jpg" nombre="Gonzalo Pagliano" empresa="Amazon" texto="Porque si no exportara de tu casa un cabello, que es grande, y nada de lo cual traería a tu casa por poco tiempo, yo sería mi hogar, o sería el más pequeño." />
+        <TarjetaTesti img="src/assets/2165947w620.jpg" nombre="Matias Lorenzatti" empresa="Apple" texto="Porque si no exportara de tu casa un cabello, que es grande, y nada de lo cual traería a tu casa por poco tiempo, yo sería mi hogar, o sería el más pequeño." />
+      </div>
     </>
   );
 }
--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import styles from './Carousel.module.css';
-const Carousel = ({titulo}) => {
 
-  
+const Carousel = ({ titulo, imagenes }) => {
   return (
-    <> 
-     <h1 className={'text-center'}>{titulo}</h1>
+    <>
+      <h1 className={'text-center'}>{titulo}</h1>
+      <div className='mx-20'>
+        <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
 
-      {/* <div className={styles.clientsslider} >
-     <div className={styles.swiperwrapper}>
-      <div className={styles.swiperslide}><img src="src/assets/hombre-haciendo-su-ritual-cuidado-personal.jpg"  /></div>
-      <div className={styles.swiperslide}><img src="src/assets/hombre-haciendo-su-ritual-cuidado-personal.jpg"  /></div>
-      <div className={styles.swiperslide}><img src="src/assets/hombre-haciendo-su-ritual-cuidado-personal.jpg"  /></div>
-      <div className={styles.swiperslide}><img src="src/assets/hombre-haciendo-su-ritual-cuidado-personal.jpg"  /></div>
-
-
-    </div>
-    <div className={styles.swiperpagination}></div>
-  </div>*/}
+          spaceBetween={50}
+          slidesPerView={4}
+          navigation
+          pagination={{ clickable: true }}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {imagenes.map((imagen, index) => (
+            <SwiperSlide className={styles.centrado} key={index}>
+              <img src={imagen} className='w-3/4' alt={`Slide ${index + 1}`} />
+            </SwiperSlide>
+          ))}
+          
+        </Swiper>
+      </div>
     </>
-    
   );
 }
 
